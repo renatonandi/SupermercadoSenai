877346df939fd5221ab9388a67d0678f733fc9ea
Renato Nandi Neto <renato.nandi@gmail.com>
2022-03-18 22:22:34 -0300

    Adicionado novo comentário

--- src/br/com/senai/supermercadosenai/entity/Imposto.java
+++ src/br/com/senai/supermercadosenai/entity/Imposto.java
@@ -1,5 +1,29 @@
 package br.com.senai.supermercadosenai.entity;
 
+import java.math.BigDecimal;
+
 public class Imposto {
+	
+	private Integer id;//Novo Comentário
+	
+	private BigDecimal valor;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	
+	
 
 }
